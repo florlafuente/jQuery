4da4056b844b575e682f40560714f95b376d9719
florlafuente <floripona@gmail.com>
2017-06-18 20:50:29 +0000

    slider intento 2

--- slider/js/functions.js
+++ slider/js/functions.js
@@ -2,25 +2,19 @@ var arraySlider = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg' ];
 var imagen = $('#imagen');
 var pos = $(imagen).attr('data-pos');
 
-
-$(document).ready(function(){
-	
-	setInterval (rotateImage, 5000);
+$(document).ready(function(){	
+	setInterval (rotateImage, 3000);
 });
 
 function rotateImage(){
   $(imagen).fadeOut('slow', function(){
 
     $(this).attr('src', arraySlider[pos]);
-
     $(this).fadeIn('slow', function(){
-      if (pos < arraySlider.length-1)
-      {
+       if (pos < arraySlider.length-1){
          pos ++;
         console.log(pos);
-      }
-      else
-      {
+      }else{
        pos = 0;
         console.log(pos);
       }
