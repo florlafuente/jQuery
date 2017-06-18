21e59a1f86b6a61834454cfba045637500822ba8
florlafuente <floripona@gmail.com>
2017-06-18 20:18:37 +0000

    slider primer intento

---
+++ slider/js/functions.js
@@ -0,0 +1,29 @@
+var arraySlider = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg' ];
+var imagen = $('#imagen');
+var pos = $(imagen).attr('data-pos');
+
+
+$(document).ready(function(){
+	
+	setInterval (rotateImage, 5000);
+});
+
+function rotateImage(){
+  $(imagen).fadeOut('slow', function(){
+
+    $(this).attr('src', arraySlider[pos]);
+
+    $(this).fadeIn('slow', function(){
+      if (pos < arraySlider.length-1)
+      {
+         pos ++;
+        console.log(pos);
+      }
+      else
+      {
+       pos = 0;
+        console.log(pos);
+      }
+    });
+  });
+}
